46c2260fb9e04bed4a2813ed7c50972a8376bc97
AldairZP <105321589+AldairZP@users.noreply.github.com>
2025-04-08 20:31:15 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function fetchDataUser() {
 
         if (response.ok) {
             const data = await response.json();
-            return !!data.username; // Devuelve true si está logueado
+            return !!data.username; // true si está logueado
         }
     } catch (error) {
         console.error('Error al verificar sesión:', error);
@@ -15,7 +15,12 @@ async function fetchDataUser() {
     return false;
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const isLoggedIn = await fetchDataUser();
-    navigateTo(isLoggedIn ? 'exams.html' : 'login-signup.html');
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('startExamBtn');
+    if (button) {
+        button.addEventListener('click', async () => {
+            const isLoggedIn = await fetchDataUser();
+            navigateTo(isLoggedIn ? 'exams.html' : 'login-signup.html');
+        });
+    }
 });
